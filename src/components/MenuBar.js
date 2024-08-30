46d8913518be5576c3e3fd612b6f507df0fb17d9
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2024-08-30 14:38:03 +0700

    menu-bar
    make menu bar that I just design

---
+++ src/components/MenuBar.js
@@ -0,0 +1,45 @@
+
+const MenuBar=()=>{
+
+    return (
+        <div className="menuBar" >
+            {/* menu */}
+            <div className="link-container">
+                <h4>
+                    Home
+                </h4>
+                <h4>
+                    Movies
+                </h4>
+                <h4>
+                    About Zeed
+                </h4>
+            </div>
+            {/* option */}
+            <div className="option-container">
+                <button>
+                    <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g id="heart-svgrepo-com 1">
+                        <path id="Vector" d="M4.01929 14.9333L15 27L25.9808 14.9333C27.2736 13.5125 28 11.5855 28 9.57614V9.23632C28 5.23982 25.0518 2 21.415 2C19.4145 2 17.5225 2.99927 16.2729 4.71582L15 6.46429L13.7271 4.71582C12.4775 2.99927 10.5855 2 8.58505 2C4.94824 2 2 5.23982 2 9.23632V9.57614C2 11.5855 2.72636 13.5125 4.01929 14.9333Z" fill="#D62828"/>
+                        </g>
+                    </svg>
+                </button>
+                <button>
+                    <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g id="heart-svgrepo-com 1">
+                        <path id="Vector" d="M4.01929 14.9333L15 27L25.9808 14.9333C27.2736 13.5125 28 11.5855 28 9.57614V9.23632C28 5.23982 25.0518 2 21.415 2C19.4145 2 17.5225 2.99927 16.2729 4.71582L15 6.46429L13.7271 4.71582C12.4775 2.99927 10.5855 2 8.58505 2C4.94824 2 2 5.23982 2 9.23632V9.57614C2 11.5855 2.72636 13.5125 4.01929 14.9333Z" fill="#D62828"/>
+                        </g>
+                    </svg>
+                </button>
+                <button>
+                    <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g id="heart-svgrepo-com 1">
+                        <path id="Vector" d="M4.01929 14.9333L15 27L25.9808 14.9333C27.2736 13.5125 28 11.5855 28 9.57614V9.23632C28 5.23982 25.0518 2 21.415 2C19.4145 2 17.5225 2.99927 16.2729 4.71582L15 6.46429L13.7271 4.71582C12.4775 2.99927 10.5855 2 8.58505 2C4.94824 2 2 5.23982 2 9.23632V9.57614C2 11.5855 2.72636 13.5125 4.01929 14.9333Z" fill="#D62828"/>
+                        </g>
+                    </svg>
+                </button>
+            </div>
+        </div>
+    )
+}
+export default MenuBar
